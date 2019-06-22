073130aab87a5fe6fbd860e25e4e1beebf2cbfdb
storres8 <stevetorres7562@gmail.com>
2019-06-22 02:31:45 -0400

    built recipe cards.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { Recipe } from "../recipe.model";
 
 @Component({
   selector: "app-recipe-list",
@@ -6,7 +7,14 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./recipe-list.component.css"]
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  // square brackets mean that it'll be an array of rececpies defines by our Recepie model.
+  recipes: Recipe[] = [
+    new Recipe(
+      "A Test Recipe",
+      "testing...",
+      "https://s3.amazonaws.com/finecooking.s3.tauntonclud.com/app/uploads/2019/03/04141012/lime-roasted-salmon-skillet-square-500x500.jpg"
+    )
+  ];
   constructor() {}
 
   ngOnInit() {}
