cdcede8e6e9cdb51ef7c991449ae3155172909f6
storres8 <stevetorres7562@gmail.com>
2019-06-25 13:33:09 -0400

    used ngIf in the navbar to determine which components to display.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from "@angular/core";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.css"]
 })
 export class HeaderComponent implements OnInit {
+  @Output() navClicked = new EventEmitter<string>();
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  handleNavClick(navData: string) {
+    this.navClicked.emit(navData);
   }
-
 }
