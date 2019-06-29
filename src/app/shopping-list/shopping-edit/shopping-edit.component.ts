fabf5dd8c6a9c4c46c1ca45908dd941f44c565cb
storres8 <stevetorres7562@gmail.com>
2019-06-28 23:29:04 -0400

    implemented the add button feature for adding new ingredients to the list of already existing ingridients.

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ElementRef,
+  ViewChild,
+  Output,
+  EventEmitter
+} from "@angular/core";
+import { Ingredient } from "src/app/shared/ingredient.model";
 
 @Component({
-  selector: 'app-shopping-edit',
-  templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.css']
+  selector: "app-shopping-edit",
+  templateUrl: "./shopping-edit.component.html",
+  styleUrls: ["./shopping-edit.component.css"]
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild("nameInput", { static: false }) nameInputref: ElementRef;
+  @ViewChild("amountInput", { static: false }) amountInputref: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  handleAdd() {
+    // Saved the values from the form as constants.
+    const ingName = this.nameInputref.nativeElement.value;
+    const ingAmount = this.amountInputref.nativeElement.value;
+    // Imported the Ingredient model and passed the name and amount variables to make a new instance.
+    const newIngredient = new Ingredient(ingName, ingAmount);
+
+    // emit a new event to send to the parent component w/ new ingredient instance.
+    this.ingredientAdded.emit(newIngredient);
   }
-
 }
